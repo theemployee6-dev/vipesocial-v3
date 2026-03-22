a8e5eb77a1b26dc6503794ba7b7c3cdac037fb44
theemployee6-dev <theemployee6@gmail.com>
2026-03-22 02:10:42 -0300

    fixed redirect to logincom google

--- src/shared/hooks/useAuth.ts
+++ src/shared/hooks/useAuth.ts
@@ -31,9 +31,8 @@ export function useAuth() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
-        // redirectTo:
-        //   "https://uninterpretively-objurgative-nerissa.ngrok-free.dev/auth/callback",
+        // redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: "https://vipesocial-v3.vercel.app/auth/v1/callback",
       },
     });
 
