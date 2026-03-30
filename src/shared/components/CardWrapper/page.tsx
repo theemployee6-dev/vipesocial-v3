1f39aa0ed3394f013f4325f5907f867c1d2a0faa
theemployee6-dev <theemployee6@gmail.com>
2026-03-30 17:54:11 -0300

    fixed cardwrapper

--- src/shared/components/CardWrapper/page.tsx
+++ src/shared/components/CardWrapper/page.tsx
@@ -11,10 +11,23 @@ export default function CardWrapper({
 }: CardWrapperProps) {
   return (
     <div
-      className={`bg-[#121212] rounded-xl p-8 md:p-10 shadow-2xl border border-white/5 relative overflow-hidden ${className}`}
+      className={`
+        relative overflow-hidden
+        bg-[#121212] rounded-xl
+        border border-white/5 shadow-2xl
+        p-6 sm:p-8 md:p-10 lg:p-12
+        ${className}
+      `}
     >
       {showCornerGlow && (
-        <div className="absolute top-0 right-0 w-32 h-32 bg-linear-to-br from-[#fe2c55]/10 blur-xl to-transparent pointer-events-none" />
+        <div
+          className="
+            absolute top-0 right-0
+            w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32
+            bg-linear-to-br from-[#fe2c55]/10 to-transparent
+            blur-xl pointer-events-none
+          "
+        />
       )}
       {children}
     </div>
